bd089cdb421d5b0dcfda417f506b745289d4f0a5
Chornyi Vlad <4ornii.vlad@gmail.com>
2022-06-08 13:31:28 +0300

    deleted line breaks

--- src/redux/auth/auth-selectors.ts
+++ src/redux/auth/auth-selectors.ts
@@ -1,14 +1,10 @@
 import { RootState } from './../store';
+
 const getIsLoggedIn = (state:RootState) => state.auth.isLoggedIn
-
 const getUserEmail = (state:RootState) => state.auth.user.email
-
 const getIsFetchingCurrent = (state:RootState) => state.auth.isFetchingCurrentUser
-
 const getAccessToken = (state:RootState) => state.auth.token
-
 const getSid = (state:RootState) => state.auth.sid
-
 const authSelectors = {
   getIsLoggedIn,
   getUserEmail,
@@ -16,4 +12,5 @@ const authSelectors = {
   getAccessToken,
   getSid,
 }
+
 export default authSelectors
